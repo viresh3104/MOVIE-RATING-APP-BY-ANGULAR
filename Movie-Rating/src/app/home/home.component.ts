a51eb462e2964c8e084bf09cade0d2595b62a1fd
viresh3104 <vireshnavtake3104@gmail.com>
2024-11-06 17:07:08 +0530

    added the movies rows based on category

--- Movie-Rating/src/app/home/home.component.ts
+++ Movie-Rating/src/app/home/home.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  constructor(private http: HttpClient) {}
 
+  ngOnInit(): void {
+    this.getbollywoodmovies();
+  }
+
+  bollywood_movies: any;
+
+  getbollywoodmovies() {
+    this.http
+      .get('http://localhost:4200/assets/movies_data/bollywood-movies.json')
+      .subscribe((movies) => {
+        this.bollywood_movies = movies;
+      });
+  }
 }
