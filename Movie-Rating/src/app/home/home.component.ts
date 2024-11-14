9a7d5422e6b1d9f25e7f6918f6d9c70e9ccd782d
viresh3104 <vireshnavtake3104@gmail.com>
2024-11-14 21:53:17 +0530

    Added the new routing to the all movies by view all button

--- Movie-Rating/src/app/home/home.component.ts
+++ Movie-Rating/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,29 +8,33 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient , private router:Router) {}
 
   ngOnInit(): void {
+    this.gethollywoodmovies();
     this.getbollywoodmovies();
-    this.getbollywoodfinal();
   }
 
+  hollywood_movies: any;
   bollywood_movies: any;
-  bollywood_final: any;
+
+  gethollywoodmovies() {
+    this.http
+      .get<any[]>('http://localhost:4200/assets/movies_data/hollywood.json')
+      .subscribe((movies) => {
+        this.hollywood_movies = movies.slice(0,8);
+      });
+  }
 
   getbollywoodmovies() {
     this.http
-      .get<any[]>('http://localhost:4200/assets/movies_data/bollywood-movies.json')
+      .get<any[]>('http://localhost:4200/assets/movies_data/bollywood.json')
       .subscribe((movies) => {
         this.bollywood_movies = movies.slice(0,8);
       });
   }
 
-  getbollywoodfinal() {
-    this.http
-      .get<any[]>('http://localhost:4200/assets/movies_data/bollywood_final.json')
-      .subscribe((movies) => {
-        this.bollywood_final = movies.slice(0,8);
-      });
+  gotomoviebars(){
+    this.router.navigate(['/movie-bars']);
   }
 }
