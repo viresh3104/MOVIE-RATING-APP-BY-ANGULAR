aa1b5cac586d44e484708397deab3c6d29605cda
viresh3104 <vireshnavtake3104@gmail.com>
2024-11-20 22:44:34 +0530

    added the funtionality to the registration page and checking some conditions for the inputs while registaring the user

--- Movie-Rating/src/app/register/register.component.ts
+++ Movie-Rating/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
-
+import { RouterModule } from '@angular/router';
 
 
 @Component({
@@ -9,9 +10,37 @@ import { Router } from '@angular/router';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  fullname = '';
+  email = '';
+  username = '';
+  password = '';
+  confirmPassword = '';
+  msg = '';
 
-  constructor(private router: Router){ }
+
+  constructor(private auth: AuthService , private router: Router) { }
+
+  ngOnInit(): void{
+
+  }
+  register(){
+    if(this.username.trim().length ===0){
+      this.msg = 'Username is required';
+    } else if(this.password.trim().length === 0){
+      this.msg = 'Password is required';
+    } else if(this.fullname.trim().length === 0){
+      this.msg = 'Fullname is required';
+    } else if(this.email.trim().length === 0){
+      this.msg = 'Email is required';
+    } else if(this.password != this.confirmPassword){
+      this.msg = 'Confirm Password not matches';
+    } 
+  }
+
   backtologin(){
     this.router.navigate(['/login']);
   } 
+  about(){
+    this.router.navigate(['./about']);
+  }
 }
