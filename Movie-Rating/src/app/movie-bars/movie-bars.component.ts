cad885fea6be26029a96aaa9a8996842737be832
viresh3104 <vireshnavtake3104@gmail.com>
2024-11-30 10:52:28 +0530

    added the routing form the home component with same ui

--- Movie-Rating/src/app/movie-bars/movie-bars.component.ts
+++ Movie-Rating/src/app/movie-bars/movie-bars.component.ts
@@ -1,25 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-bars',
   templateUrl: './movie-bars.component.html',
-  styleUrl: './movie-bars.component.css'
+  styleUrl: './movie-bars.component.css',
 })
 export class MovieBarsComponent {
-  constructor(private http : HttpClient) { }
-  
-  ngOnInit() : void{
-    this.getbollywoodmovies();
+  movielist: any[] = [];
+  gonera: string = '';
+
+  constructor(
+    private http: HttpClient,
+    private router: ActivatedRoute,
+    private route: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.gonera = this.router.snapshot.paramMap.get('gonera')!;
+
+    this.fetchMoviesByGonera(this.gonera);
   }
 
-  bollywood_movies: any;
-
-  getbollywoodmovies(){
-    this.http
-    .get<any[]>('http://localhost:4200/assets/movies_data/bollywood.json')
-    .subscribe((movies) => {
-      this.bollywood_movies = movies;
-      });
+  fetchMoviesByGonera(gonera: string): void {
+    if (gonera === 'bollywood') {
+      this.http
+        .get<any[]>('http://localhost:4200/assets/movies_data/bollywood.json')
+        .subscribe((movies) => {
+          this.movielist = movies;
+        });
+    } else if (gonera === 'hollywood') {
+      this.http
+        .get<any[]>('http://localhost:4200/assets/movies_data/hollywood.json')
+        .subscribe((movies) => {
+          this.movielist = movies;
+        });
     }
+  }
+
+  gotomoviedetails(movie: any) {
+    console.log(movie);
+    this.route.navigate(['/movie'], { queryParams: { moviedetails: movie } });
+  }
 }
