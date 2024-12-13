f58eaecfe72c24b96376c15822e40d0f42a92ff0
viresh3104 <vireshnavtake3104@gmail.com>
2024-12-13 19:11:45 +0530

    new child component for the slider animation for better maintaince and debug

---
+++ Movie-Rating/src/app/home/movie-slider/movie-slider.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-slider',
+  templateUrl: './movie-slider.component.html',
+  styleUrl: './movie-slider.component.css'
+})
+export class MovieSliderComponent {
+
+}
